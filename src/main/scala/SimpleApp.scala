8f213f58131bcfd5680cc392fe1705bf0b3c8b9b
David Medina Ospina <dameos1@gmail.com>
2017-11-14 11:26:53 -0500

    Dude, are you reading this?

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -1,6 +1,5 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
-import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
 import org.apache.spark.rdd.RDD
 
 
@@ -8,6 +7,7 @@ object Hi {
     def main(args: Array[String]) = {
         val conf = new SparkConf ().setAppName("word count")
         val sc = new SparkContext(conf)
-        val lines = sc.textFile("hdfs://datasets/gutenberg")
+        val files = sc.textFile("hdfs://datasets/gutenberg")
+        println(files.partitions.length)
     }
 }
