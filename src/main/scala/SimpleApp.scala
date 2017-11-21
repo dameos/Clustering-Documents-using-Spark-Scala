3a1df509032fb9a358b686174bbdddb6372c0b2b
David Medina Ospina <dameos1@gmail.com>
2017-11-21 17:16:53 -0500

    adding comment

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -10,24 +10,34 @@ import org.apache.spark.ml.Pipeline
 object Proyecto4 {
 	
     def main(args: Array[String]){
-        case class Data(path: String, text:String)
-        val conf = new SparkConf().setAppName("word count")
-        val sc = new SparkContext(conf)
+        // Se Define el nombre de la aplicación
+        val conf = new SparkConf().setAppName("Clustering Spark") 
+        // Se carga la variable de contexto de spark
+        val sc = new SparkContext(conf) 
+        // Se crea la sesión para spark SQL
         val spark = SparkSession.builder().getOrCreate()
+
+        //Se crea el struct para la lectura de los archivos (path, texto)
         val struct = StructType(
                         StructField("path", StringType, true)::
                         StructField("text", StringType, true):: Nil)
-
+        
+        // Se leen los archivos y se convierten a Row(path, text) para la creación del dataframe
     	val files = sc.wholeTextFiles("hdfs:///user/dmedina7/datasets/gutenberg/*.txt").map(data => Row(data._1, data._2))
-        val df = spark.createDataFrame(files, struct)
-        df.show()
+        val dataFrame = spark.createDataFrame(files, struct)
+        dataFrame.show()
 
+        // Se tokenizan los texto
         val tokenizer = new Tokenizer()
             .setInputCol("text")
             .setOutputCol("words")
+        
+        // Se quitan las stopWords para hacer más preciso el Peso TF-IDF
         val remover = new StopWordsRemover()
             .setInputCol(tokenizer.getOutputCol)
             .setOutputCol("cleanedWords")
+
+        // Se hacen las estimación TF-IDF que describe el articulo del enunciado de la práctica   
         val hashingTF = new HashingTF()
             .setNumFeatures(1000)
             .setInputCol(remover.getOutputCol)
@@ -36,14 +46,22 @@ object Proyecto4 {
             .setInputCol(hashingTF.getOutputCol)
             .setOutputCol("newFeatures")
             .setMinDocFreq(3)
+        
+        // Se hace el KMeans con un k de 4 
         val kmeans = new KMeans()
             .setK(4)
+        
+        // Se usan Pipelines para definar las etapas de limpieza, peso y clustering de documentos
         val pipeline = new Pipeline()
             .setStages(Array(tokenizer, remover, hashingTF, idf, kmeans))
         
-        val model = pipeline.fit(df)
-        val results = model.transform(df)
-        results.write.json("hdfs:///user/dmedina7/resultados")
+        // Se encaja el modelo para seguir la pipeline
+        val model = pipeline.fit(dataFrame)
+
+        // Se transforma el modelo para seguir el pipeline
+        val results = model.transform(dataFrame)
+
+        // Los resultados son mostrados en el stdout
         results.show()
    }
 }
