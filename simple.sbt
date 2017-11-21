1e7ef7219c007283319bec32a8277b9d97c30bd6
David Medina Ospina <dameos1@gmail.com>
2017-11-21 11:49:26 -0500

    Dude, are you reading this?

--- simple.sbt
+++ simple.sbt
@@ -8,7 +8,8 @@ libraryDependencies ++= {
   val sparkVer = "2.1.0"
   Seq(
     "org.apache.spark" %% "spark-core" % sparkVer % "provided" withSources(),
-    "org.apache.spark" %% "spark-mllib" % "2.0.1",
-    "org.apache.spark" %% "spark-mllib-local" % "2.0.1"
+    "org.apache.spark" %% "spark-mllib" % sparkVer,
+    "org.apache.spark" %% "spark-mllib-local" % sparkVer,
+    "org.apache.spark" %% "spark-sql" % sparkVer
   )
 }
--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -1,29 +1,48 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
-import org.apache.spark.rdd.RDD
-import org.apache.spark.mllib.feature.HashingTF
-import org.apache.spark.mllib.linalg.Vector
-import org.apache.spark.mllib.feature.IDF
-import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.feature.{HashingTF, IDF, Tokenizer, StopWordsRemover}
+import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.Pipeline
 
-object Hi {
+object Proyecto4 {
 	
     def main(args: Array[String]){
+        case class Data(path: String, text:String)
         val conf = new SparkConf().setAppName("word count")
         val sc = new SparkContext(conf)
-    	val files = sc.textFile("hdfs:///user/dmedina7/datasets/gutenberg/*.txt").map(_.split(" ").toSeq)
-	    val hashingTF = new HashingTF()
-	    val tf: RDD[Vector] = hashingTF.transform(files)
-	    tf.cache()
-    	val idf = new IDF().fit(tf)
-    	val tfidf: RDD[Vector] = idf.transform(tf)
-        val model = KMeans.train(tfidf, 4, 20)
+        val spark = SparkSession.builder().getOrCreate()
+        val struct = StructType(
+                        StructField("path", StringType, true)::
+                        StructField("text", StringType, true):: Nil)
 
-        val cost = model.computeCost(tfidf)
-        println("Cost = " + cost)
+    	val files = sc.wholeTextFiles("hdfs:///user/dmedina7/datasets/gutenberg/*.txt").map(data => Row(data._1, data._2))
+        val df = spark.createDataFrame(files, struct)
+        df.show()
+
+        val tokenizer = new Tokenizer()
+            .setInputCol("text")
+            .setOutputCol("words")
+        val remover = new StopWordsRemover()
+            .setInputCol(tokenizer.getOutputCol)
+            .setOutputCol("cleanedWords")
+        val hashingTF = new HashingTF()
+            .setNumFeatures(1000)
+            .setInputCol(remover.getOutputCol)
+            .setOutputCol("features")
+        val idf = new IDF()
+            .setInputCol(hashingTF.getOutputCol)
+            .setOutputCol("newFeatures")
+            .setMinDocFreq(3)
+        val kmeans = new KMeans()
+            .setK(4)
+        val pipeline = new Pipeline()
+            .setStages(Array(tokenizer, remover, hashingTF, idf, kmeans))
         
-        println("Cluster Centers: ")
-        val centers = model.clusterCenters
-        centers.foreach(println)
+        val model = pipeline.fit(df)
+        val results = model.transform(df)
+        results.show()
    }
 }
